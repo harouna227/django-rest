17870cbf6bed9c28f3dbb61f420e7872d0f1ae00
harouna227 <issifiharouna07@gmail.com>
2024-12-03 07:20:48 +0300

    headers, content-type, params, post-data, loads, dumps

--- api/views.py
+++ api/views.py
@@ -1,10 +1,15 @@
+import json
 from django.shortcuts import render
 from django.http import JsonResponse
 
 # Create your views here.
 def api_view(request, *args, **kwargs):
-    data = {
-        'nom': 'haroun',
-        'profile': 'developpeur python'
-    }
+    print(request.body) # retourne une chaine binaire
+    data = json.loads(request.body) # traduit en json
+    origin_data = json.dumps(data) # donnees d'origine
+    data['headers'] = dict(request.headers)
+    data['params'] = dict(request.GET)
+    data['post-data'] = dict(request.POST)
+    print(request.headers)
+    data['Conten_type'] = request.content_type
     return JsonResponse(data=data)
--- client/base.py
+++ client/base.py
@@ -2,6 +2,7 @@ import requests
 
 # Point d'entree
 endpoint = " http://127.0.0.1:8000/api/"
-response = requests.get(endpoint)
+response = requests.get(endpoint, params={'hh': 22}, json={'query': 'hello'})
+# response = requests.get(endpoint, data={'query': 'hello'})
 print(response.json())
 print(response.status_code) 
